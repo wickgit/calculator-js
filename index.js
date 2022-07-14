514f609bb637bfe7379124220401df4d54a5c644
Markiyan <melnik.markiyan@gmail.com>
2022-07-14 18:05:08 +0300

    Fixed buggs and added new functionality

--- index.js
+++ index.js
@@ -2,25 +2,21 @@ const display = document.querySelector('.display');
 const container = document.querySelector('.container');
 const operators = document.querySelector('.operators');
 const evaluate = document.getElementById('result');
+const clearButton = document.getElementById('clear');
+const displayUpper = document.querySelector('.display_upper');
 
 let firstNumber = '';
 let secondNumber = '';
-let evaluatedNumber;
+let allOperations = '';
 let operation = '';
 let isOperatorOn = false;
 let evaluationResult;
 
-function add(a, b) {
-    return a + b;
-}
+function add(a, b) { return a + b; }
 
-function substract(a, b) {
-    return a - b;
-}
+function substract(a, b) { return a - b; }
 
-function multiply(a, b) {
-    return a * b;
-}
+function multiply(a, b) { return a * b; }
 
 function divide(a, b) {
     if (b == 0) return 'Can\'t divide by zero';
@@ -38,6 +34,25 @@ function operate(operation, a, b) {
         return divide(a, b);
     }
 }
+
+function clear() {
+    operation = '';
+    firstNumber = '';
+    secondNumber = '';
+    evaluationResult = '';
+    display.innerHTML = '';
+    isOperatorOn = false;
+    displayUpper.textContent = '';
+}
+
+function addToDisplay(str) {
+    if (str === 'clear') {
+        displayUpper.textContent = '';
+        return;
+    }
+    displayUpper.textContent += str + ' ';
+}
+
 function displayNumbers(e) {
     if (evaluationResult) {
         display.innerHTML = '';
@@ -50,32 +65,47 @@ function displayNumbers(e) {
             secondNumber += e.target.textContent;
         }
     }
-    console.log('First number: ', firstNumber, 'Second number:', secondNumber, operation);
+    console.log('First number: ', firstNumber, 'Second number:', secondNumber, 'Operation:', operation);
 }
 
 function displayOperators(e) {
     if (e.target.className == 'operator_button') {
-        operation = e.target.textContent
+        if (!firstNumber && !secondNumber) return;
         if (firstNumber && !isOperatorOn) {
             display.innerHTML = '';
             operation = e.target.textContent;
             isOperatorOn = true;
+            addToDisplay(firstNumber);
+            addToDisplay(operation);
             firstNumber = Number(firstNumber)
             console.log(operation);
         }
+        if (firstNumber && secondNumber) {
+            secondNumber = Number(secondNumber);
+            evaluationResult = operate(operation, firstNumber, secondNumber);
+            display.innerHTML = `${evaluationResult}`;
+            console.log('відповідь', evaluationResult, 'Operation:', operation);
+            operation = '';
+            firstNumber = '';
+            secondNumber = '';
+            isOperatorOn = true;
+            firstNumber = evaluationResult;
+        }
+        operation = e.target.textContent
+        
     }
 }
 
 function displayResult(e) {
-    if (firstNumber && secondNumber ) {
+    if (firstNumber && secondNumber) {
         secondNumber = Number(secondNumber);
         evaluationResult = operate(operation, firstNumber, secondNumber);
-        display.innerHTML = `${evaluationResult}`;
-        console.log(evaluationResult);
+        addToDisplay('clear');
+        addToDisplay(evaluationResult);
         operation = '';
         firstNumber = '';
         secondNumber = '';
-        isOperatorOn = false;
+        display.innerHTML = `${evaluationResult}`; 
         firstNumber = evaluationResult;
     }
 }
@@ -83,3 +113,4 @@ function displayResult(e) {
 container.addEventListener('click', displayNumbers);
 operators.addEventListener('click', displayOperators);
 evaluate.addEventListener('click', displayResult);
+clearButton.addEventListener('click', clear);
